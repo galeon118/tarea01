fc044c84b701ffaf004a1711ddbbbaa4b9d79996
Gabriel Leon <galeon@unal.edu.co>
2022-03-22 13:23:13 -0500

    Tarea01C

--- 2022-I-Prog/2022-03-oddinv/Tarea-oddsum-Gl.cpp
+++ 2022-I-Prog/2022-03-oddinv/Tarea-oddsum-Gl.cpp
@@ -2,14 +2,14 @@
 #include <cmath>
 int main (void)
 {
-  std::cout <<"Gabriel Leon \n" << "El valor de pi haciendo uso de la sumatoria 1/n^2 con n=1 a n=10000 es:\n";
+  std::cout <<"Gabriel Leon \n" << "Suma de los inversos impares entre 10 y 100\n";
   double result = 0;
   int N = 45;
   int n = 0;
   for( n = 1; n <= N   ; n = n+1    ) {
     result = (result + 1.0/(2*n+9));
   }  
-  std::cout<< result << "\n";
+  std::cout<<"Resultado="<< result << "\n";
   return 0;
  
 } 
